8302c5b3dfade5a96da85926110b51bad88605aa
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-12 22:45:25 +1200

    refactor: Update GameController to handle user input and implement game logic

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -12,8 +12,11 @@ public class Game {
   private HumanPlayer user;
   private AiPlayer ai;
   private PlayerFactory playerFactory;
+  private boolean result;
+  private Choice choice;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
+    this.choice = choice;
     game = new GameController();
     game.resetGame();
     name = options[0];
@@ -27,6 +30,7 @@ public class Game {
     game.startNewGame();
     userInput = user.makeMove();
     aiInput = ai.makeMove();
+    result = game.whoWins(userInput, aiInput, choice);
   }
 
   public void endGame() {}
--- src/main/java/nz/ac/auckland/se281/GameController.java
+++ src/main/java/nz/ac/auckland/se281/GameController.java
@@ -1,10 +1,26 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Main.Choice;
+
 public class GameController {
   private int totalGamePlayed;
+  private String userInput;
+  private String aiInput;
 
   public GameController() {
     this.totalGamePlayed = 0;
+  }
+
+  public boolean whoWins(String userInput, String aiInput, Choice choice) {
+    this.userInput = userInput;
+    this.aiInput = aiInput;
+    if (choice == Choice.ODD) {
+
+    } else {
+
+    }
+
+    return true;
   }
 
   public void startNewGame() {
