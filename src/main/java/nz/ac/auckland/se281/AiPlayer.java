44c916dca1c3576fe149c35a8ee1b95b7a16008b
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-12 22:30:11 +1200

    feat: Add method to update strategy and increment wins for AiPlayer

--- src/main/java/nz/ac/auckland/se281/AiPlayer.java
+++ src/main/java/nz/ac/auckland/se281/AiPlayer.java
@@ -2,9 +2,21 @@ package nz.ac.auckland.se281;
 
 public class AiPlayer implements Player {
   private GameStrategy strategy;
+  private int playerConsecutiveWins;
 
   public AiPlayer(GameStrategy strategy) {
     this.strategy = strategy;
+  }
+
+  public void updateStrategy(GameStrategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void incrementWins() {
+    playerConsecutiveWins++;
+    if (playerConsecutiveWins >= 3 && this.strategy instanceof RandomStrategy) {
+      updateStrategy(new TopStrategy());
+    }
   }
 
   @Override
