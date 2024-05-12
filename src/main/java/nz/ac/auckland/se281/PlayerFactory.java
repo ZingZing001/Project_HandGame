70df1ab8b583bcff7e4cc3f99f4c253d959bd0c0
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-12 20:10:41 +1200

    Implemented Strategy design pattern and factory design pattern for AiPlayer

---
+++ src/main/java/nz/ac/auckland/se281/PlayerFactory.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se281;
+
+public class PlayerFactory {
+  public Player creatPlayer(String difficulty) {
+    switch (difficulty) {
+      case "easy":
+        return new AiPlayer(new RandomStrategy());
+      default:
+        break;
+    }
+  }
+}
