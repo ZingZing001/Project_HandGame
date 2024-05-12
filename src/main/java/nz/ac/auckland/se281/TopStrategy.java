a6b5f9880acf0e5698955326cd9b8241efc34b77
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-13 00:04:29 +1200

    refactor: Update TopStrategy to calculate sum of user choices and return random number based on the sum

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -1,10 +1,29 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Main.Choice;
+
 public class TopStrategy extends Game implements GameStrategy {
+  private int OddSum;
+  private int EvenSum;
+
+  public TopStrategy() {
+    OddSum = 0;
+    EvenSum = 0;
+  }
 
   @Override
   public String excuteStrategy() {
-
-    return "-1";
+    for (Choice choice : userChoices) {
+      if (choice == Choice.ODD) {
+        OddSum++;
+      } else {
+        EvenSum++;
+      }
+    }
+    if (OddSum > EvenSum) {
+      return Utils.getRandomEvenNumber() + "";
+    } else {
+      return Utils.getRandomOddNumber() + "";
+    }
   }
 }
