4883e1a24c6f828047128c0ef372c71359808108
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:12:55 +1200

    refactor: Update GameStrategy interface with JavaDoc and improve code readability

--- src/main/java/nz/ac/auckland/se281/GameStrategy.java
+++ src/main/java/nz/ac/auckland/se281/GameStrategy.java
@@ -1,5 +1,12 @@
 package nz.ac.auckland.se281;
 
+/** Represents a strategy that can be used by a player in the game. */
 public interface GameStrategy {
+
+  /**
+   * Executes the strategy and returns the player's move as a string.
+   *
+   * @return the player's move as a string.
+   */
   String excuteStrategy();
 }
