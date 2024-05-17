8f9a0bf20adc4d125816543c167d0374d20cc237
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:17:41 +1200

    refactor: Improve TopStrategy class with JavaDoc and code readability

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -3,38 +3,50 @@ package nz.ac.auckland.se281;
 import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.Choice;
 
+/** A strategy that generates a move for the AI player based on the player's previous choices. */
 public class TopStrategy implements GameStrategy {
-  private int OddSum;
-  private int EvenSum;
+  private int oddSum;
+  private int evenSum;
   private ArrayList<Choice> choices;
   private Choice gameChoice;
   private int getRan;
 
+  /**
+   * Constructs a TopStrategy with the specified player choices and game choice.
+   *
+   * @param userChoices the list of player's previous choices.
+   * @param gameChoice the current game choice.
+   */
   public TopStrategy(ArrayList<Choice> userChoices, Choice gameChoice) {
     this.choices = userChoices;
     this.gameChoice = gameChoice;
   }
 
+  /**
+   * Executes the top strategy and returns the generated move.
+   *
+   * @return the generated move as a string.
+   */
   @Override
   public String excuteStrategy() {
     for (Choice choice : choices) {
       if (choice == Choice.ODD) {
-        OddSum++;
+        oddSum++;
       } else {
-        EvenSum++;
+        evenSum++;
       }
     }
-    if (OddSum > EvenSum && gameChoice == Choice.ODD) {
+    if (oddSum > evenSum && gameChoice == Choice.ODD) {
       return Utils.getRandomOddNumber() + "";
-    } else if (EvenSum > OddSum && gameChoice == Choice.EVEN) {
+    } else if (evenSum > oddSum && gameChoice == Choice.EVEN) {
       return Utils.getRandomOddNumber() + "";
-    } else if (OddSum > EvenSum && gameChoice == Choice.EVEN) {
+    } else if (oddSum > evenSum && gameChoice == Choice.EVEN) {
       getRan = Utils.getRandomEvenNumber();
       while (getRan == 0) {
         getRan = Utils.getRandomEvenNumber();
       }
       return getRan + "";
-    } else if (EvenSum > OddSum && gameChoice == Choice.ODD) {
+    } else if (evenSum > oddSum && gameChoice == Choice.ODD) {
       getRan = Utils.getRandomEvenNumber();
       while (getRan == 0) {
         getRan = Utils.getRandomEvenNumber();
