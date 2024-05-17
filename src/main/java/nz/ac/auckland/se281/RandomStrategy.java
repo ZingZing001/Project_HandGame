49a24832ddecd8483ee8232e6b233872a541d523
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:16:20 +1200

    refactor: Improve RandomStrategy class with JavaDoc and code readability

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -1,9 +1,15 @@
 package nz.ac.auckland.se281;
 
+/** A strategy that generates a random move for the AI player. */
 public class RandomStrategy implements GameStrategy {
 
   private int random;
 
+  /**
+   * Executes the random strategy and returns the generated move.
+   *
+   * @return the generated move as a string.
+   */
   @Override
   public String excuteStrategy() {
     random = Utils.getRandomNumberRange(0, 5);
