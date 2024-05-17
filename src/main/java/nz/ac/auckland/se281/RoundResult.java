35526b2a8729a81173b7022571535994a5fae9c9
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:04:12 +1200

    refactor: Improve RoundResult class with JavaDoc and code readability

--- src/main/java/nz/ac/auckland/se281/RoundResult.java
+++ src/main/java/nz/ac/auckland/se281/RoundResult.java
@@ -1,12 +1,23 @@
 package nz.ac.auckland.se281;
 
+/** Represents the result of a single round in the game. */
 public class RoundResult extends Game {
   private boolean humanWon;
 
+  /**
+   * Constructs a RoundResult with the specified outcome.
+   *
+   * @param humanWon true if the human player won the round, false otherwise.
+   */
   public RoundResult(boolean humanWon) {
     this.humanWon = humanWon;
   }
 
+  /**
+   * Checks if the human player won the round.
+   *
+   * @return true if the human player won, false otherwise.
+   */
   public boolean isHumanWon() {
     return humanWon;
   }
