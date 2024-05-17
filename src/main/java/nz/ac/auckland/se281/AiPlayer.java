f1ba9e34673a2e8faa7177233359002bcd5e72d8
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:11:38 +1200

    refactor: Update AiPlayer class to improve code readability and add JavaDoc

--- src/main/java/nz/ac/auckland/se281/AiPlayer.java
+++ src/main/java/nz/ac/auckland/se281/AiPlayer.java
@@ -3,20 +3,38 @@ package nz.ac.auckland.se281;
 import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.Choice;
 
+/** Represents an AI player in the game, using different strategies to make moves. */
 public class AiPlayer extends Game implements Player {
   private final String name;
   private GameStrategy strategy;
   private String aiInput;
 
+  /**
+   * Constructs an AI player with the specified initial strategy.
+   *
+   * @param strategy the initial strategy for the AI player.
+   */
   public AiPlayer(GameStrategy strategy) {
     name = "HAL-9000";
     this.strategy = strategy;
   }
 
+  /**
+   * Updates the AI player's strategy.
+   *
+   * @param strategy the new strategy for the AI player.
+   */
   public void updateStrategy(GameStrategy strategy) {
     this.strategy = strategy;
   }
 
+  /**
+   * Swaps the AI player's strategy between RandomStrategy and TopStrategy based on the current
+   * strategy.
+   *
+   * @param choices the list of choices available.
+   * @param gameChoice the current game choice.
+   */
   public void swapStrategy(ArrayList<Choice> choices, Choice gameChoice) {
     if (strategy instanceof RandomStrategy) {
       updateStrategy(new TopStrategy(choices, gameChoice));
@@ -25,6 +43,11 @@ public class AiPlayer extends Game implements Player {
     }
   }
 
+  /**
+   * Makes a move using the current strategy and returns the AI player's input.
+   *
+   * @return the AI player's input as a string.
+   */
   @Override
   public String makeMove() {
     aiInput = strategy.excuteStrategy();
