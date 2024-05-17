9681b3bb7aa3487637f133f319fc1925fb87da6d
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:13:45 +1200

    refactor: Update HumanPlayer class with JavaDoc and improve code readability

--- src/main/java/nz/ac/auckland/se281/HumanPlayer.java
+++ src/main/java/nz/ac/auckland/se281/HumanPlayer.java
@@ -1,18 +1,30 @@
 package nz.ac.auckland.se281;
 
+/** Represents a human player in the game. */
 public class HumanPlayer implements Player {
   private String name;
   private String userInput;
   private InputHandler input = new InputHandler();
 
+  /**
+   * Constructs a HumanPlayer with the specified name.
+   *
+   * @param name the name of the human player.
+   */
   public HumanPlayer(String name) {
     this.name = name;
   }
 
+  /** Greets the human player with a welcome message. */
   public void Greet() {
     MessageCli.WELCOME_PLAYER.printMessage(name);
   }
 
+  /**
+   * Prompts the human player for their move and returns the input.
+   *
+   * @return the human player's input as a string.
+   */
   @Override
   public String makeMove() {
     MessageCli.ASK_INPUT.printMessage();
