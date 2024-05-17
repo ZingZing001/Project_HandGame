22d3c5df54695417b1ff874242edf80705081597
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-17 14:21:23 +1200

    refactor: Improve InputHandler class with JavaDoc and code readability

--- src/main/java/nz/ac/auckland/se281/InputHandler.java
+++ src/main/java/nz/ac/auckland/se281/InputHandler.java
@@ -1,14 +1,14 @@
 package nz.ac.auckland.se281;
 
-/** Class for validify inputs from the user */
+/** Class for validify inputs from the user. */
 public class InputHandler {
   private String input;
 
   /**
    * This method checks whether if the userInput's validity and whether it matches with the
-   * condition. It will keeps checking until the user gives out the correct form of input
+   * condition. It will keeps checking until the user gives out the correct form of input.
    *
-   * @return returns the input character
+   * @return returns the input character.
    */
   public String propmtForFirstCharacter() {
     input = Utils.scanner.nextLine();
